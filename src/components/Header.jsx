4b0b48b3f6c0265e55bf521816ab261b346a9e59
Sharath <sharath.esr@gmail.com>
2025-01-27 11:20:48 +0530

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+            <img src="src/assets/Chef Claude Icon.png" alt="chef-icon"/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
