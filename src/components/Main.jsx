61c58fa43ab39bfeff56c036f300c4e70910fbfe
Sharath <sharath.esr@gmail.com>
2025-01-28 19:44:43 +0530

    refactor: update form Submission from onSubmit to action handler

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,17 +6,14 @@ export default function Main() {
     <li key={ingredient}>{ingredient}</li>
   ));
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formEl = event.currentTarget;
-    const formData = new FormData(formEl);
+  function handleSubmit(formData) {
     const newIngredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
