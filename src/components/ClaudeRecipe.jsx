56af5d65d6d21b634f5cc1030352da8fca081e08
Sharath <sharath.esr@gmail.com>
2025-01-31 00:52:09 +0530

    style: update styles for recipe instructions

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,6 +1,11 @@
-export default function ClaudeRecipe(props){
-  
-    return (
-      <section>{props.recipe}</section>
-    )
+import ReactMarkdown from "react-markdown";
+
+export default function ClaudeRecipe(props) {
+  console.log(props.recipe)
+  return (
+    <section className="suggested-recipe-container"  aria-live="polite">
+      <h2>Chef Claude Recommends:</h2>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
+    </section>
+  );
 }
